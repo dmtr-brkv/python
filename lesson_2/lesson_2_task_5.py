f43c7bddbc20ee618db9cd7754147887247ecb3d
dmtr-brkv <dmtr-brkv@mail.ru>
2025-07-06 13:35:57 +0300

    lesson 2

---
+++ lesson_2/lesson_2_task_5.py
@@ -0,0 +1,13 @@
+month = int(input("Введите номер месяца (1-12): "))
+def month_to_season(month):
+    if (1 <= month <= 2) or 12 :
+        return "Зима"
+    elif 3 <= month <= 5:
+        return "Весна"
+    elif 6 <= month <= 8:
+        return "Лето"
+    elif 9 <= month <= 11:
+        return "Осень"
+    else:
+        return "Неверный номер месяца"
+print(month_to_season(month))
